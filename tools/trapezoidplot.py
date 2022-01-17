b01c0e2cb82e3c62cd140cd537941048079f40c6
HerrHorizontal <maximilian.horzela@kit.edu>
2022-01-17 16:36:42 +0100

    update trapezoidplot

--- tools/trapezoidplot.py
+++ tools/trapezoidplot.py
@@ -17,7 +17,16 @@ import os.path
 import glob
 import json
 
+
 njobs = 60
+scenario = "copy"
+suffix="test"
+
+scenario_plotlabel_dict = {
+    "copy": "Input-files copied",
+    "simplifiedstream": "Input-files streamed (simpl.)",
+    "fullstream": "Block-streaming"
+}
 
 machine_color_dict = {
     'sg01': 'red',
@@ -26,48 +35,54 @@ machine_color_dict = {
 }
 
 # create a dict of hitrate and corresponding simulation-trace JSON-output-files
-outputfiles = glob.glob(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "sgbatch", "tmp", "outputs", f"unified*{njobs}jobs.json")))
-hitrates = [float(outfile.split("_")[1].strip("h")) for outfile in outputfiles]
+output_dir = os.path.join(os.path.dirname(__file__), "..", "sgbatch", "tmp", "outputs")
+print(f"Searching for simulation traces in {output_dir}")
+outputfiles = glob.glob(os.path.abspath(os.path.join(output_dir, f"hitratescaling_{scenario}_{njobs}jobs_hitrate*.csv")))
+
+print("Found {} output-files!".format(len(outputfiles)))
+hitrates = [float(outfile.split("_")[-1].strip(".csv").strip("hitrate")) for outfile in outputfiles]
+
 outputfiles_dict = dict(zip(hitrates,outputfiles))
+print(outputfiles_dict)
+
 
 # create a trapez plot for each JSON file corresponding to a hitrate value
-fractions = pd.Series([0., 1., 1., 0.])
+fractions = pd.Series([0., 0.5, 1., 0.])
 for hitrate, outputfile in outputfiles_dict.items():
     with open(outputfile) as f:
         # create a dataframe from JSON 
-        df_tmp = pd.json_normalize(json.load(f)['workflow_execution']['tasks'])
+        df_tmp = pd.read_csv(f, sep=',\t', engine='python')
 
-    # filter dataframe for necessary information
-    df_tmp = df_tmp.filter(
-        items=["whole_task.start", "whole_task.end", "compute.start", "compute.end", "execution_host.hostname"]
-    )
+    # compute end points and compute streaming share time
+    df_tmp["compute.end"] = df_tmp["job.end"] - df_tmp["outfiles.transfertime"]
+    df_tmp["transferonly.share"] = df_tmp["compute.end"] - df_tmp["job.computetime"]
 
     # plot the trapezoid
     fig, ax = plt.subplots()
-    ax.set_title("hitrate {}".format(hitrate))
+    ax.set_title(f"hitrate {hitrate}")
     ax.set_xlabel('time / s', loc='right')
     ax.set_xlim([-100.,6100.])
     tmp_label = ""
     for index, row in df_tmp.iterrows():
-        if row.filter(items=['execution_host.hostname'])[0] != tmp_label:
+        if row.filter(items=['machine.name'])[0] != tmp_label:
             ax.plot(
                 row.filter(
-                    items=["whole_task.start", "compute.start", "compute.end", "whole_task.end"]
+                    items=["job.start", "transferonly.share", "compute.end", "job.end"]
                 ),
                 fractions, 
                 linewidth=0.1, 
-                color=machine_color_dict[row.filter(items=['execution_host.hostname'])[0]],
-                label = row.filter(items=['execution_host.hostname'])[0] 
+                color=machine_color_dict[row.filter(items=['machine.name'])[0]],
+                label = row.filter(items=['machine.name'])[0] 
             )
-            tmp_label = row.filter(items=['execution_host.hostname'])[0]
+            tmp_label = row.filter(items=['machine.name'])[0]
         else:
             ax.plot(
                 row.filter(
-                    items=["whole_task.start", "compute.start", "compute.end", "whole_task.end"]
+                    items=["job.start", "transferonly.share", "compute.end", "job.end"]
                 ),
                 fractions, 
                 linewidth=0.1, 
-                color=machine_color_dict[row.filter(items=['execution_host.hostname'])[0]]
+                color=machine_color_dict[row.filter(items=['machine.name'])[0]]
             )
 
     h1, l1 = ax.get_legend_handles_labels()
@@ -76,4 +91,4 @@ for hitrate, outputfile in outputfiles_dict.items():
     h1 = list(legend_dict.values())
     ax.legend(h1, l1, loc=1)
 
-    fig.savefig("trapezoid_{}jobs_hitrate{}.pdf".format(njobs, hitrate))
+    fig.savefig(f"trapezoid_{njobs}{scenario}jobs_hitrate{hitrate}.pdf")
