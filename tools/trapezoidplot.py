772cabcc8b88874d53cba541ea2e71aa2ca73e0c
HerrHorizontal <maximilian.horzela@kit.edu>
2022-01-18 15:33:41 +0100

    add argument parsing

--- tools/trapezoidplot.py
+++ tools/trapezoidplot.py
@@ -15,12 +15,46 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os.path
 import glob
-import json
+import argparse
 
 
-njobs = 60
-scenario = "copy"
-suffix="test"
+parser = argparse.ArgumentParser(
+    description="Produce a plot for each file depicting the lifetime of all jobs of the corresponding\
+        simulation using a trapezoidal representation. \
+        The files containing the simulation dump are CSV files produced by the output method of the simulator.",
+    add_help=True
+)
+parser.add_argument(
+    "--njobs",
+    type=int,
+    default=None,
+    help="Optional check for the number of jobs in the passed simulation runs.\
+        Will also be present in the name of the output plot."
+)
+parser.add_argument(
+    "--scenario", 
+    type=str,
+    choices=("copy", "simplifiedstream", "fullstream"),
+    required=True,
+    help="Choose a scenario, which sets the according plotting label and filename of the plot."
+)
+parser.add_argument(
+    "--suffix",
+    help="Optonal suffix to add to the plots' file-names."
+)
+parser.add_argument(
+    "simoutputs",
+    nargs='+',
+    help="CSV files containing information about the simulated jobs \
+        produced by the simulator."
+)
+
+
+args = parser.parse_args()
+
+njobs = args.njobs
+scenario = args.scenario
+suffix=args.suffix
 
 scenario_plotlabel_dict = {
     "copy": "Input-files copied",
@@ -35,23 +69,23 @@ machine_color_dict = {
 }
 
 # create a dict of hitrate and corresponding simulation-trace JSON-output-files
-output_dir = os.path.join(os.path.dirname(__file__), "..", "sgbatch", "tmp", "outputs")
-print(f"Searching for simulation traces in {output_dir}")
-outputfiles = glob.glob(os.path.abspath(os.path.join(output_dir, f"hitratescaling_{scenario}_{njobs}jobs_hitrate*.csv")))
+outputfiles = args.simoutputs
 
-print("Found {} output-files!".format(len(outputfiles)))
+print("Found {0} output-files! Producing {0} trapezoidal plots...".format(len(outputfiles)))
 hitrates = [float(outfile.split("_")[-1].strip(".csv").strip("hitrate")) for outfile in outputfiles]
 
 outputfiles_dict = dict(zip(hitrates,outputfiles))
 print(outputfiles_dict)
 
 
-# create a trapez plot for each JSON file corresponding to a hitrate value
+# create a trapez plot for each CSV file corresponding to a hitrate value
 fractions = pd.Series([0., 0.5, 1., 0.])
 for hitrate, outputfile in outputfiles_dict.items():
     with open(outputfile) as f:
-        # create a dataframe from JSON 
+        # create a dataframe from CSV 
         df_tmp = pd.read_csv(f, sep=',\t', engine='python')
+        if njobs!=None:
+            assert(df_tmp.shape[0]==njobs)
 
     # compute end points and compute streaming share time
     df_tmp["compute.end"] = df_tmp["job.end"] - df_tmp["outfiles.transfertime"]
@@ -91,4 +125,9 @@ for hitrate, outputfile in outputfiles_dict.items():
     h1 = list(legend_dict.values())
     ax.legend(h1, l1, loc=1)
 
-    fig.savefig(f"trapezoid_{njobs}{scenario}jobs_hitrate{hitrate}.pdf")
+    plotfilename = "trapezoid"
+    if njobs:
+        plotfilename += f"{njobs}{scenario}jobs_hitrate{hitrate}"
+    if suffix:
+        plotfilename += f"{suffix}.pdf"
+    fig.savefig(plotfilename)
