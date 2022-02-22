cd108c4e9e95b7dd1c5da8138db0f31babe8d135
Artur Gottmann <artur.gottmann@kit.edu>
2022-02-22 19:31:48 +0100

    tools/simscaling.py: extend with extrapolations

--- tools/simscaling.py
+++ tools/simscaling.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+import scipy.optimize
 from matplotlib import pyplot as plt
 import os.path
 import glob
@@ -23,12 +24,22 @@ scenario_plotlabel_dict = {
     'withdump': "with JSON dump",
     'nodump': "without JSON dump",
     'private': "private improvements",
-    "hacky": "final hacky-WRENCH"
+    "hacky": "final hacky-WRENCH",
+    "wrench2": "WRENCH 2.0"
 }
 
 
 def converttime(df: pd.DataFrame, a: str, b: str):
     return df[a].astype(int)*60 + df[b].astype(int)
+
+def timeexp(x, m, t, b, c):
+    return m * np.exp(- t * x + c) + b
+
+def timep1(x, m, b):
+    return m * x + b
+
+def timep2(x, m, m2, b):
+    return m * x + b + m2 * x**2
 
 
 parser = argparse.ArgumentParser(
@@ -41,7 +52,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "--scenario", 
     type=str,
-    choices=("withdump", "nodump", "private", "hacky"),
+    choices=("withdump", "nodump", "private", "hacky", "wrench2"),
     required=True,
     help="Choose a scenario, which sets the according plotting label and filename of the plot."
 )
@@ -103,12 +114,12 @@ ax.set_title("Simulation scaling " + scenario_plotlabel_dict[scenario])
 
 # ax.set_xscale('log')
 ax.set_xlabel('$N_{jobs}$', loc='right')
-ax.set_ylabel('time / min', color='black')
-ax.set_xlim([0,2100])
+ax.set_ylabel('time / min', color='cornflowerblue')
+ax.set_xlim([0,runtimesdf['NJobs'].iloc[-1]*1.05])
 # ax.set_ylim([0,400])
 
-ax.plot(runtimesdf['NJobs'], runtimesdf['TIME']/60, linestyle='dotted', color='black')
-ax.scatter(runtimesdf['NJobs'], runtimesdf['TIME']/60, color='black', marker='x', label='runtime')
+ax.plot(runtimesdf['NJobs'], runtimesdf['TIME']/60, linestyle='dotted', color='cornflowerblue')
+ax.scatter(runtimesdf['NJobs'], runtimesdf['TIME']/60, color='cornflowerblue', marker='x', label='runtime')
 ax.grid(axis="y", linestyle = 'dotted', which='major')
 
 secax = ax.twinx()
@@ -121,6 +132,30 @@ secax.set_ylabel('memory / GiB', color='orange')
 h1, l1 = ax.get_legend_handles_labels()
 h2, l2 = secax.get_legend_handles_labels()
 ax.legend(h1+h2, l1+l2, loc=2)
+
+njobs = np.linspace(runtimesdf['NJobs'].iloc[0], runtimesdf['NJobs'].iloc[-1], 1000)
+
+#start_params = (1., 1., 1., 1.)
+#params, cv = scipy.optimize.curve_fit(timeexp, runtimesdf['NJobs'], runtimesdf['TIME']/60, start_params)
+#m, t, b, c = params
+#print(params)
+#ax.plot(njobs, timeexp(njobs, m, t, b, c), linestyle='-', color='black', label='runtime extrapolation')
+#print(f"100k extrapolation exp: {timeexp(100000, m, t, b, c)} min")
+
+#start_params = (0.01, 0.01)
+#params, cv = scipy.optimize.curve_fit(timep1, runtimesdf['NJobs'], runtimesdf['TIME']/60, start_params)
+#m, b = params
+#print(params)
+#ax.plot(njobs, timep1(njobs, m, b), linestyle='-', color='black', label='runtime extrapolation')
+#print(f"100k extrapolation linear: {timep1(100000, m, b)} min")
+
+start_params = (0.01, 0.01, 0.01)
+params, cv = scipy.optimize.curve_fit(timep2, runtimesdf['NJobs'], runtimesdf['TIME']/60, start_params)
+m, m2, b = params
+print(params)
+#ax.plot(njobs, timep2(njobs, m, m2, b), linestyle='-', color='black', label='runtime extrapolation')
+print(f"100k extrapolation a_1 * x + a_2 * x**2 + b: {timep2(100000, m, m2, b)} min = {timep2(100000, m, m2, b)/60.} h = {timep2(100000, m, m2, b)/60./24.} d")
+
 
 fig.savefig("scalingtest_"+ scenario +".pdf")
 
