80aa3a2f2c7d9e9a2d722d54b6611c033c615e66
HerrHorizontal <maximilian.horzela@kit.edu>
2022-03-29 15:21:59 +0200

    minor improvements and add options

--- tools/hitratescan.sh
+++ tools/hitratescan.sh
@@ -3,24 +3,50 @@
 # Bash script tu run simulations for several hitrate values.
 # The simulation monitoring outputs can be plotted using the hitrateperfromance.py plotting script.
 
-NJOBS=60
-NINFILES=10
-AVGINSIZE=3600000000
-FLOPS=216442800000
+action() {
+    # determine the directy of this file
+    if [ ! -z "$ZSH_VERSION" ]; then
+        local this_file="${(%):-%x}"
+    else
+        local this_file="${BASH_SOURCE[0]}"
+    fi
+    #source /cvmfs/cms.cern.ch/slc6_amd64_gcc480/external/python/2.7.3/etc/profile.d/init.sh
 
-SCENARIO="fullstream" # further options synchronized with plotting script "copy", "simplifiedstream", "fullstream"
+    local base="$( cd "$( dirname "$this_file" )" && pwd )"
+    local parent="$( dirname "$base" )"
 
-OUTDIR="tmp/outputs"
-if [ ! -d $OUTDIR ]; then
-    mkdir -p $OUTDIR
-fi
+    local PLATFORM_DIR="$parent/data/platform-files"
+    local PLATFORM="sgbatch.xml"
 
-for hitrate in $(LANG=en_US seq 0.0 0.1 1.0)
-do 
-    sgbatch-sim --platform data/platform-files/hosts.xml \
-        --njobs $NJOBS \
-        --ninfiles $NINFILES --insize $AVGINSIZE \
-        --hitrate ${hitrate} \
-        --flops $FLOPS \
-        --output-file ${OUTDIR}/hitratescaling_${SCENARIO}_${NJOBS}jobs_hitrate${hitrate}.csv
-done
+    local NJOBS=60
+    local NINFILES=10
+    local AVGINSIZE=3600000000
+    local FLOPS=216442800000
+    local SIGMA_FLOPS=0
+    local SIGMA_MEM=0
+    local SIGMA_INSIZE=0
+    local SIGMA_OUTSIZE=0
+
+    local SCENARIO="fullstream" # further options synchronized with plotting script "copy", "simplifiedstream", "fullstream"
+
+    local OUTDIR="$parent/tmp/outputs"
+    if [ ! -d $OUTDIR ]; then
+        mkdir -p $OUTDIR
+    fi
+
+    for hitrate in $(LANG=en_US seq 0.0 0.1 1.0)
+    do 
+        sgbatch-sim --platform "$PLATFORM_DIR/$PLATFORM" \
+            --njobs $NJOBS \
+            --ninfiles $NINFILES --insize $AVGINSIZE \
+            --sigma-insize $SIGMA_INSIZE \
+            --hitrate ${hitrate} \
+            --flops $FLOPS \
+            --sigma-flops $SIGMA_FLOPS \
+            --sigma-mem $SIGMA_MEM \
+            --sigma-outsize $SIGMA_OUTSIZE \
+            --output-file ${OUTDIR}/hitratescaling_${SCENARIO}_${NJOBS}jobs_hitrate${hitrate}.csv
+    done
+}
+
+action "$@"
