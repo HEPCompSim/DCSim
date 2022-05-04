890ccdc4423f60373670831096be45f1dcd5c7d4
HerrHorizontal <maximilian.horzela@kit.edu>
2022-05-04 13:47:33 +0200

    remove stray todo

--- src/computation/CopyComputation.h
+++ src/computation/CopyComputation.h
@@ -10,7 +10,6 @@
 class CopyComputation : public CacheComputation {
 
 public:
-    // TODO: REMOVE MOST THINGS IN HERE AND RELY ON THE GLOBALS IN SimpleSimulation::...
     CopyComputation(
         std::set<std::shared_ptr<wrench::StorageService>> &cache_storage_services,
         std::set<std::shared_ptr<wrench::StorageService>> &grid_storage_services,
