a6285b11f4e85068e46cd4f8619c679b0f29d3cf
HerrHorizontal <maximilian.horzela@kit.edu>
2022-06-30 17:17:22 +0200

    always chose the right debug monitor files

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -2,5 +2,5 @@
 
 bash tools/hitratescan_debug.sh
 pushd tmp/outputs/
-python3 ~/DistCacheSim/tools/hitrateperformance.py --scenario fullstream --suffix "_validationSGbatch_CPUbound" hitratescaling_fullstream_48jobs_hitrate*.csv 
+python3 ~/DistCacheSim/tools/hitrateperformance.py --scenario fullstream --suffix "_validationSGbatch_CPUbound" hitratescaling_fullstream_*jobs_hitrate*.csv 
 popd
