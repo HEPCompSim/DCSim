59768e5824967133154672441cddc23fc3ecf644
HerrHorizontal <maximilian.horzela@kit.edu>
2022-06-30 16:56:30 +0200

    run the right debug script

--- run_debug_test.sh
+++ run_debug_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-bash tools/hitratescan.sh
+bash tools/hitratescan_debug.sh
 pushd tmp/outputs/
 python3 ~/DistCacheSim/tools/hitrateperformance.py --scenario fullstream --suffix "_validationSGbatch_CPUbound" hitratescaling_fullstream_48jobs_hitrate*.csv 
 popd
