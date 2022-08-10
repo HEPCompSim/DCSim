e32425a1445e32411205de58f5d79fd2ba7b4187
HerrHorizontal <maximilian.horzela@kit.edu>
2022-08-10 14:34:33 +0200

    more granular scan and better printouts

--- tools/simscaling.sh
+++ tools/simscaling.sh
@@ -14,21 +14,21 @@ if [ ! -d "tmp/monitor/$SCENARIO" ]; then
     mkdir -p tmp/monitor/$SCENARIO
 fi
 
-for NJOBS in 10 20 50 100 200 500 1000 1200 1500 2000 2500 3000 
+for NJOBS in 10 20 50 100 200 500 700 1000 1100 1200 1300 1500 1700 2000 2500 3000 
 do
     sgbatch-sim --platform data/platform-files/ETPbatch.xml \
         --njobs ${NJOBS} --ninfiles ${NINFILES} --insize ${AVGINSIZE} \
         --hitrate 0.0 \
         --output-file /dev/null \
     & TEST_PID=$!
-    echo $TEST_PID
 
     (while [[ True ]]; \
         do ps -aux | grep " ${TEST_PID} " | grep "sgbatch-sim" \
         >> tmp/monitor/$SCENARIO/test_privatedump_${NJOBS}jobs.txt; \
         sleep 10; done;)\
     & MONITOR_PID=$!
-    echo $TEST_PID $MONITOR_PID
+    echo "Simulation process to monitor: $TEST_PID"
+    echo "Monitoring process: $MONITOR_PID"
 
     wait $TEST_PID
     kill -9 ${MONITOR_PID}
