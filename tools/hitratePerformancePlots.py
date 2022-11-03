36dfb31caea0eed509fadee555f77495ca4028bc
HerrHorizontal <maximilian.horzela@kit.edu>
2022-11-03 09:38:48 +0100

    seaborn plotting

--- tools/hitratePerformancePlots.py
+++ tools/hitratePerformancePlots.py
@@ -3,12 +3,26 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
 import os.path
 import argparse
 
 
-plt.rcParams["figure.figsize"] = [4., 3.]
-plt.rcParams["figure.autolayout"] = True
+plt.rcParams['figure.autolayout'] = True
+pd.set_option('display.max_columns',None)
+plt.rcParams['axes.facecolor'] = 'white'
+plt.rcParams['axes.spines.left'] = False
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.spines.bottom'] = False
+plt.rcParams['axes.grid'] = False
+plt.rcParams['axes.grid.axis'] = 'both'
+plt.rcParams['axes.labelcolor'] = '#555555'
+plt.rcParams['text.color'] = 'black'
+plt.rcParams['figure.figsize'] = 6,4
+plt.rcParams['figure.dpi'] = 100
+plt.rcParams['figure.titleweight'] = 'normal'
+plt.rcParams['font.family'] = 'sans-serif'
 
 
 scenario_plotlabel_dict = {
@@ -22,8 +36,8 @@ scenario_plotlabel_dict = {
 
 def valid_file(param):
     base, ext = os.path.splitext(param)
-    if ext.lower() not in ('.csv'):
-        raise argparse.ArgumentTypeError('File must have a csv extension')
+    if ext.lower() not in (".csv"):
+        raise argparse.ArgumentTypeError("File must have a csv extension")
     return param
 
 
@@ -60,9 +74,6 @@ scenario = args.scenario
 suffix=args.suffix
 
 
-machines = ['sg01', 'sg03', 'sg04']
-
-
 # create a dict of hitrate and corresponding simulation-trace JSON-output-files
 outputfiles = args.simoutputs
 for outputfile in outputfiles:
@@ -95,76 +106,49 @@ else:
     exit(1)
 
 
-# plot the job-runtime dependence on hitrate
-fig, ax1 = plt.subplots()
-ax1.set_title(scenario_plotlabel_dict[scenario])
+# Derive quantities
+df["Walltime"] = (df["job.end"]-df["job.start"])/60
+df["IOtime"] = (df["infiles.transfertime"]+df["outfiles.transfertime"])/60
+df["Efficiency"] = df["job.computetime"]/(df["job.end"]-df["job.start"])
 
-ax1.set_xlabel('hitrate', loc='right')
-ax1.set_ylabel('jobtime / min', color='black')
+# plot and save
+
+fig = plt.figure("hitrate-walltime", figsize=(6,4))
+
+ax1 = sns.scatterplot(x="hitrate", y="Walltime", hue="machine.name", data=df, alpha=0.9)
+ax1.set_title(scenario_plotlabel_dict[scenario])
+ax1.set_xlabel("hitrate", loc="right")
+ax1.set_ylabel("jobtime / min", color="black")
 ax1.set_xlim([-0.05,1.05])
 # ax1.set_ylim([20,65])
-
-# ax1 = df.plot.scatter(x='hitrate', y='walltime', c=)
-
-scatter = ax1.scatter(
-    df['hitrate'], ((df['job.end']-df['job.start'])/60), 
-    c=df['machine.name'].astype('category').cat.codes, 
-    marker='x'
-)
-# ax1.grid(axis="y", linestyle = 'dotted', which='major')
-
-ax1.legend(
-    handles=scatter.legend_elements()[0], 
-    labels=machines,
-    title="host"
-)
+ax1.legend(loc='best')
 
 fig.savefig(f"hitrateWalltime_{scenario}jobs{suffix}.pdf")
 fig.savefig(f"hitrateWalltime_{scenario}jobs{suffix}.png")
 
 
-fig2, ax2 = plt.subplots()
+fig2 = plt.figure("hitrate-transfertime", figsize=(6,4))
+
+ax2 = sns.scatterplot(x="hitrate", y="IOtime", hue="machine.name", data=df, alpha=0.9)
 ax2.set_title(scenario_plotlabel_dict[scenario])
-
-ax2.set_xlabel('hitrate', loc='right')
-ax2.set_ylabel('transfer time / min', color='black')
+ax2.set_xlabel("hitrate", loc="right")
+ax2.set_ylabel("transfer time / min", color="black")
 ax2.set_xlim([-0.05,1.05])
-
-scatter = ax2.scatter(
-    df['hitrate'], ((df['infiles.transfertime']+df['outfiles.transfertime'])/60), 
-    c=df['machine.name'].astype('category').cat.codes, 
-    marker='x'
-)
-
-ax2.legend(
-    handles=scatter.legend_elements()[0], 
-    labels=machines,
-    title="host"
-)
+ax2.legend(loc='best')
 
 fig2.savefig(f"hitrateIOtime_{scenario}jobs{suffix}.pdf")
 fig2.savefig(f"hitrateIOtime_{scenario}jobs{suffix}.png")
 
 
-fig3, ax3 = plt.subplots()
-ax3.set_title(scenario_plotlabel_dict[scenario])
+fig3= plt.figure("hitrate-efficiency", figsize=(6,4))
 
-ax3.set_xlabel('hitrate', loc='right')
-ax3.set_ylabel('CPU eff.', color='black')
+ax3 = sns.scatterplot(x="hitrate", y="Efficiency", hue="machine.name", data=df, alpha=0.9)
+ax3.set_title(scenario_plotlabel_dict[scenario])
+ax3.set_xlabel("hitrate", loc="right")
+ax3.set_ylabel("CPU eff.", color="black")
 ax3.set_xlim([-0.05,1.05])
 ax3.set_ylim([0,1.05])
-
-scatter = ax3.scatter(
-    df['hitrate'], (df['job.computetime']/(df['job.end']-df['job.start'])), 
-    c=df['machine.name'].astype('category').cat.codes, 
-    marker='x'
-)
-
-ax3.legend(
-    handles=scatter.legend_elements()[0], 
-    labels=machines,
-    title="host"
-)
+ax3.legend(loc='best')
 
 fig3.savefig(f"hitrateCPUeff_{scenario}jobs{suffix}.pdf")
 fig3.savefig(f"hitrateCPUeff_{scenario}jobs{suffix}.png")
