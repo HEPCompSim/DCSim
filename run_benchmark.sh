7f0d93b224a5582278de5206a0b474c1be1357a2
Henri Casanova <henric@hawaii.edu>
2022-12-01 11:03:11 -1000

    fix

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,12 +1,12 @@
 if [ ! $# -eq 4 ]; then
-    echo "Usage: ${0} <# of jobs> <# of files per job> <executable> <buffer size>"
+    echo "Usage: ${0} <# of jobs> <# of files per job> <xrootd block size> <buffer size>"
     exit 1
 fi
 
 
 NJOBS="${1}"
 NINFILES="${2}"
-EXEC="${3}"
+XRDBLOCKSIZE="${3}"
 BUFFERSIZE="${4}"
 AVGINSIZE=$(bc -l <<< "8554379000/20")
 SIGMAINSIZE=$(bc -l <<< "10000")
@@ -17,12 +17,11 @@ OUTSIZE=$(bc -l <<< "50000000")
 SIGMAOUTSIZE=$(bc -l <<< "1000000")
 DUPLICATIONS=1
 HITRATE=0.05
-XRDBLOCKSIZE=1000000
 
 
 PLATFORM="../data/platform-files/ETPbatch_faster"
 
-./${EXEC} --platform ${PLATFORM}.xml \
+LD_PRELOAD=/usr/local/lib/libprofiler.so CPUPROFILE=prof_${NJOBS}_${XRDBLOCKSIZE}_${BUFFERSIZE}.out ./dc-sim --platform ${PLATFORM}.xml \
         --njobs ${NJOBS} --ninfiles ${NINFILES} --insize ${AVGINSIZE} --sigma-insize ${SIGMAINSIZE} \
         --flops ${FLOPS} --sigma-flops ${SIGMAFLOPS} --mem ${MEM} \
         --outsize ${OUTSIZE} --sigma-outsize ${SIGMAOUTSIZE} \
@@ -30,5 +29,6 @@ PLATFORM="../data/platform-files/ETPbatch_faster"
         --hitrate 0.0 \
  	--storage-buffer-size ${BUFFERSIZE} \
         --xrd-blocksize ${XRDBLOCKSIZE} \
-        --output-file ${PLATFORM}${NJOBS}.csv  
- 
+        --output-file ${PLATFORM}${NJOBS}.csv 
+
+pprof --text `pwd`/dc-sim prof_${NJOBS}_${XRDBLOCKSIZE}_${BUFFERSIZE}.out > prof_${NJOBS}_${XRDBLOCKSIZE}_${BUFFERSIZE}.txt
