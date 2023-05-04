b87254489cbe91e835a839587bc8450cd1081073
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2023-05-03 16:49:51 -1000

    Update platformFromVector.py

--- tools/platformFromVector.py
+++ tools/platformFromVector.py
@@ -11,9 +11,9 @@ with open(xml_file_path, 'r') as f:
     xml_contents = f.read()
 
 # Replace the placeholders in the XML file with the specified values
-xml_contents = re.sub(r'{CPU_SPEED}', cpu_speed, xml_contents)
-xml_contents = re.sub(r'{READ_SPEED}', read_speed, xml_contents)
-xml_contents = re.sub(r'{LINK_SPEED}', link_speed, xml_contents)
-xml_contents = re.sub(r'{NET_SPEED}', net_speed, xml_contents)
+xml_contents = re.sub(r'{cpu-speed}', cpu_speed, xml_contents)
+xml_contents = re.sub(r'{read-speed}', read_speed, xml_contents)
+xml_contents = re.sub(r'{link-speed}', link_speed, xml_contents)
+xml_contents = re.sub(r'{net-speed}', net_speed, xml_contents)
 
 print(xml_contents)
