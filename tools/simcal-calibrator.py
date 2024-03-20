9752ee98814351e707e0277628c2e55adb51adb0
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-03-19 17:31:36 -1000

    Create simcal-calibrator.py

---
+++ tools/simcal-calibrator.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+import argparse
+import atexit
+import csv
+import json
+import os
+import sys
+from collections import defaultdict
+from statistics import mean, stdev, StatisticsError
+
+import simcal as sc
+
+
+def dataLoader(scsn, fcsn, scfn, fcfn):
+    with open(scsn, 'r') as file:
+        scsn = json.load(file)
+    with open(fcsn, 'r') as file:
+        fcsn = json.load(file)
+    with open(scfn, 'r') as file:
+        scfn = json.load(file)
+    with open(fcfn, 'r') as file:
+        fcfn = json.load(file)
+    return scsn, fcsn, scfn, fcfn
+
+
+class Simulator(sc.Simulator):
+    def __init__(self, path, xml_template):
+        super().__init__()
+        self.path = path
+        with open(xml_template, 'r') as f:
+            self.template = f.read()
+
+    def extract(self, outfile):
+        hitrate_data = defaultdict(list)
+        with open(outfile, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                hitrate_data[row["machine.name"]].append(row)
+        return hitrate_data
+
+    def run(self, env, args):
+        # Args structure
+        # {
+        #     "platform",
+        #     "hitrate",
+        #     "xrootd_blocksize",
+        #     "network_blocksize",
+        #     "workload",
+        #     "output"
+        # }
+
+        # self.bash(path, str(jargs))
+        env.bash(self.path,
+                 args=(
+                     str(jargs),
+                     "--platform", args["platform"],
+                     "--output-file", args["output"],
+                     "--workload-configurations", args["workload"][0],
+                     "--dataset-configurations", args["workload"][1],
+                     "--hitrate", args["hitrate"],
+                     "--xrd-blocksize", args["xrootd_block"],
+                     "--storage-buffer-size", args["network_blocksize"],
+                     "--duplications", "48",
+                     "--cfg=network/loopback-bw:100000000000000",
+                     "--no-caching"
+                 ),
+                 std_in=())
+        return self.extract(output)
+
+
+class SamplePoint:
+    def __init__(self, simulator, hitrates, xrootd_blocksize, network_blocksize, workloads):
+        self.simulator = simulator
+        self.hitrates = hitrates
+        self.xrootd_blocksize = xrootd_blocksize
+        self.network_blocksize = network_blocksize
+        self.workloads = workloads
+
+    def fill_template(self, env, args):
+        # Get the command-line arguments
+        xml_contents = self.template
+
+        # Replace the placeholders in the XML file with the specified values
+        xml_contents = re.sub(r'{cpu-speed}', str(args["cpuSpeed"]), xml_contents)
+        xml_contents = re.sub(r'{read-speed}', str(args["cacheSpeed"]), xml_contents)
+        xml_contents = re.sub(r'{link-speed}', str(args["internalNetworkSpeed"]), xml_contents)
+        xml_contents = re.sub(r'{net-speed}', str(args["externalNetworkSpeed"]), xml_contents)
+
+        platform = env.tmp_file(encoding='utf8')
+        platform.write(xml_contents)
+        platform.flush()
+        return platform
+
+    def restructure(self, inter):
+        # inter[workload][hitrate]{jobtag,machine,[data]}
+        # to
+        # ret[workload][machine][hitrate]{data} 
+        ret = defaultdict(dict)
+        for workload, hitrates in inter.items():
+            for hitrate, machines in hitrates.items():
+                for machine, data in machines.items():
+                    ret[workload][machine][hitrate] = data
+        return ret
+
+    def call_platform(self, env, args):
+        inter = {}
+        platform = fill_template(env, args)
+        for workload in self.workloads:
+            inter[workload] = {}
+            for hitrate in self.hitrates:
+                inter[workload][hitrate] = self.simulator((workload, platform, hitrate), env=env)
+        platform.close()
+        return self.restructure(inter)
+
+    def __call__(self, args):
+        env = sc.Environment()
+        env.tmp_dir(".")
+        scsn = self.call_platform(env, (
+            {"cpuSpeed": args["cpuSpeed"],
+             "cacheSpeed": args["disk"],
+             "internalNetworkSpeed": args["internalNetwork"],
+             "externalNetworkSpeed": args["externalSlowNetwork"]
+             }))
+        fcsn = self.call_platform(env, (
+            {"cpuSpeed": args["cpuSpeed"],
+             "cacheSpeed": args["ramDisk"],
+             "internalNetworkSpeed": args["internalNetwork"],
+             "externalNetworkSpeed": args["externalSlowNetwork"]
+             }))
+        fcfn = self.call_platform(env, (
+            {"cpuSpeed": args["cpuSpeed"],
+             "cacheSpeed": args["ramDisk"],
+             "internalNetworkSpeed": args["internalNetwork"],
+             "externalNetworkSpeed": args["externalFastNetwork"]
+             }))
+        scfn = self.call_platform(env, (
+            {"cpuSpeed": args["cpuSpeed"],
+             "cacheSpeed": args["disk"],
+             "internalNetworkSpeed": args["internalNetwork"],
+             "externalNetworkSpeed": args["externalFastNetwork"]
+             }))
+        return scsn, scfn, fcsn, fcfn
+
+
+def loss(reference, simulated):
+    return 0
+
+
+# do whatever
+data = dataLoader("../../DCSIM calibration Data/individualSlowRawData.json",
+                  "../../DCSIM calibration Data/individualFastRawData.json",
+                  "../../DCSIM calibration Data/duplicateSlowRawData.json",
+                  "../../DCSIM calibration Data/duplicateFastRawData.json"
+                  )
+simulator = Simulator("tools/hitrateScanScript.sh",
+                      "../data/platform-files/sgbatch_validation_template.xml")
+calibrator = sc.calibrators.Debug()
+calibrator.add_param("cpuSpeed", "flops").exponential_range(20, 40)
+calibrator.add_param("ramdisk", "Bps").exponential_range(20, 40)
+calibrator.add_param("disk", "Bps").exponential_range(20, 40)
+calibrator.add_param("internalNetwork", "bps").exponential_range(20, 40)
+calibrator.add_param("externalFastNetwork", "bps").exponential_range(20, 40)
+calibrator.add_param("externalSlowNetwork", "bps").exponential_range(20, 40)
+
+point = SamplePoint(simulator)
+calibrator.calibrate(point, loss, data)
