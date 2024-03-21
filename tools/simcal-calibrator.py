d9004cc3cde1bcb9ee30a6279fc63cd7f08acd6f
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-03-20 17:47:58 -1000

    Update simcal-calibrator.py

--- tools/simcal-calibrator.py
+++ tools/simcal-calibrator.py
@@ -150,23 +150,27 @@ class SamplePoint:
 def loss(reference, simulated):
     return 0
 
+try{
+	# do whatever
+	data = dataLoader("../../DCSIM calibration Data/individualSlowRawData.json",
+					  "../../DCSIM calibration Data/individualFastRawData.json",
+					  "../../DCSIM calibration Data/duplicateSlowRawData.json",
+					  "../../DCSIM calibration Data/duplicateFastRawData.json"
+					  )
+	simulator = Simulator("dc-sim")
+	calibrator = sc.calibrators.Debug(sys.stdout)
 
-# do whatever
-data = dataLoader("../../DCSIM calibration Data/individualSlowRawData.json",
-                  "../../DCSIM calibration Data/individualFastRawData.json",
-                  "../../DCSIM calibration Data/duplicateSlowRawData.json",
-                  "../../DCSIM calibration Data/duplicateFastRawData.json"
-                  )
-simulator = Simulator("dc-sim")
-calibrator = sc.calibrators.Debug(sys.stdout)
+	calibrator.add_param("cpuSpeed", sc.parameter.Exponential(20, 40).format("%.2f flops"))
+	calibrator.add_param("ramdisk", sc.parameter.Exponential(20, 40).format("%.2f Bps"))
+	calibrator.add_param("disk", sc.parameter.Exponential(20, 40).format("%.2f Bps"))
+	calibrator.add_param("internalNetwork", sc.parameter.Exponential(20, 40).format("%.2f bps"))
+	calibrator.add_param("externalFastNetwork", sc.parameter.Exponential(20, 40).format("%.2f bps"))
+	calibrator.add_param("externalSlowNetwork", sc.parameter.Exponential(20, 40).format("%.2f bps"))
 
-calibrator.add_param("cpuSpeed", sc.parameter.Exponential(20, 40).format("%.2f flops"))
-calibrator.add_param("ramdisk", sc.parameter.Exponential(20, 40).format("%.2f Bps"))
-calibrator.add_param("disk", sc.parameter.Exponential(20, 40).format("%.2f Bps"))
-calibrator.add_param("internalNetwork", sc.parameter.Exponential(20, 40).format("%.2f bps"))
-calibrator.add_param("externalFastNetwork", sc.parameter.Exponential(20, 40).format("%.2f bps"))
-calibrator.add_param("externalSlowNetwork", sc.parameter.Exponential(20, 40).format("%.2f bps"))
-
-dataDir=toolsDir/"../data"
-point = SamplePoint(simulator,"../data/platform-files/sgbatch_validation_template.xml", [1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.0], 1_000_000_000, 0, ((dataDir/"dataset-configs/crown_ttbar_testjob.json",dataDir/"workload-configs/crown_ttbar_testjob.json"),))
-calibrator.calibrate(point, loss, data)
+	dataDir=toolsDir/"../data"
+	point = SamplePoint(simulator,"../data/platform-files/sgbatch_validation_template.xml", [1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.0], 1_000_000_000, 0, ((dataDir/"dataset-configs/crown_ttbar_testjob.json",dataDir/"workload-configs/crown_ttbar_testjob.json"),))
+	calibrator.calibrate(point, loss, data)
+except Exception e:
+	print(e)
+	while True:
+		pass:
