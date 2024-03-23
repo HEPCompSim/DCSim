9c2c552d3d5352ca43f5358dbe59b0b72a6deebf
Henri Casanova <henric@hawaii.edu>
2024-03-22 14:30:48 -1000

    updated benchmark script

--- tools/run_benchmark.sh
+++ tools/run_benchmark.sh
@@ -4,7 +4,8 @@
 #
 #
 parent="$( dirname "$base" )"
-PLATFORM="$parent/data/platform-files/WLCG_disklessTier2.xml"
+PLATFORM1="$parent/data/platform-files/WLCG_disklessTier2.xml"
+PLATFORM2="$parent/data/platform-files/WLCG_disklessTier2_MODIFIED.xml"
 WORKLOAD="$parent/data/workload-configs/simScaling.json"
 DATASET="$parent/data/dataset-configs/simScaling.json"
 DUPLICATIONS=1
@@ -20,34 +21,37 @@ if [ ! -d "tmp/monitor/$SCENARIO" ]; then
 fi
 
 
-for NJOBS in 1
+for NJOBS in 400
 do
-    for XRDBLOCKSIZE in 1000000 10000000 100000000 500000000 1000000000 5000000000
-    #for XRDBLOCKSIZE in 10000000 5000000000
-    #for XRDBLOCKSIZE in 1000000000000000
+    for XRDBLOCKSIZE in 1000000000
     do
-	echo "XRDBLOCKSIZE: $XRDBLOCKSIZE"
-    	WORKLOAD_TMP="${WORKLOAD}.tmp" 
-    	cp $WORKLOAD $WORKLOAD_TMP
-    	DATASET_TMP="${DATASET}.tmp"
-    	cp $DATASET $DATASET_TMP
-    	NFILES=$((NJOBS*1))
-    	sed -i "" "s/\"num_jobs\": [0-9]*,/\"num_jobs\": $NJOBS,/" "$WORKLOAD_TMP"
-    	sed -i "" "s/\"num_files\": [0-9]*,/\"num_files\": $NFILES,/" "$DATASET_TMP"
-	OUTFILE=/tmp/benchmark_xrtd_$XRDBLOCKSIZE.stdout
-	ERRFILE=/tmp/benchmark_xrtd_$XRDBLOCKSIZE.stderr
-    	/opt/local/bin/gtime -v dc-sim --platform "$PLATFORM" \
-        	--duplications ${DUPLICATIONS} \
-        	--hitrate 0.0 \
-		--storage-buffer-size $BUFFER_SIZE \
-        	--xrd-blocksize ${XRDBLOCKSIZE} \
-        	--output-file /dev/null \
-        	--workload-configurations "$WORKLOAD_TMP" \
-        	--dataset-configurations "$DATASET_TMP"  --wrench-full-log 1> $OUTFILE 2> $ERRFILE
-    	cat $ERRFILE | grep -e "done" | sed "s/S.* /$NJOBS jobs took /" | sed "s/$/ sec/"
-    	cat $ERRFILE | grep -e "Elap" | sed "s/.*):/  - Elapsed:    /"
-    	cat $ERRFILE | grep -e "Maxi" | sed "s/.*):/  - MaxRSS (kb):/"
-	echo "Detailed RSS info: cat $ERRFILE | grep CURRENT"
+	for PLATFORM in $PLATFORM1 $PLATFORM2 
+	do
+		echo "PLATFORM: $PLATFORM"
+		PLATFORM_NAME=`echo $PLATFORM | sed "s/.*\///"`
+    		WORKLOAD_TMP="${WORKLOAD}.tmp" 
+    		cp $WORKLOAD $WORKLOAD_TMP
+    		DATASET_TMP="${DATASET}.tmp"
+    		cp $DATASET $DATASET_TMP
+    		NFILES=$((NJOBS*20))
+    		sed -i "" "s/\"num_jobs\": [0-9]*,/\"num_jobs\": $NJOBS,/" "$WORKLOAD_TMP"
+    		sed -i "" "s/\"num_files\": [0-9]*,/\"num_files\": $NFILES,/" "$DATASET_TMP"
+		OUTFILE=/tmp/benchmark_xrtd_$PLATFORM_NAME.stdout
+		ERRFILE=/tmp/benchmark_xrtd_$PLATFORM_NAME.stderr
+    		/opt/local/bin/gtime -v dc-sim --platform "$PLATFORM" \
+        		--duplications ${DUPLICATIONS} \
+        		--hitrate 0.0 \
+			--storage-buffer-size $BUFFER_SIZE \
+			--wrench-commport-pool-size=100000 \
+        		--xrd-blocksize ${XRDBLOCKSIZE} \
+        		--output-file /dev/null \
+        		--workload-configurations "$WORKLOAD_TMP" \
+        			--dataset-configurations "$DATASET_TMP"  --wrench-full-log 1> $OUTFILE 2> $ERRFILE
+    		cat $ERRFILE | grep -e "done" | sed "s/S.* /$NJOBS jobs took /" | sed "s/$/ sec/"
+    		cat $ERRFILE | grep -e "Elap" | sed "s/.*):/  - Elapsed:    /"
+    		cat $ERRFILE | grep -e "Maxi" | sed "s/.*):/  - MaxRSS (kb):/"
+		echo "Detailed RSS info: cat $ERRFILE | grep CURRENT"
    
+		done
 	done
 done
