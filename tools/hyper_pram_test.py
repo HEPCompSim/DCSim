e68eb4a921d43af8e70e21178067b3fab569c37d
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2024-05-23 17:04:28 -1000

    Update hyper_pram_test.py

--- tools/hyper_pram_test.py
+++ tools/hyper_pram_test.py
@@ -27,35 +27,35 @@ if __name__=="__main__":
 	#calibrator = sc.calibrators.Grid()
 	#calibrator = sc.calibrators.Random()
 	dataDir=toolsDir/"../data"
-	samplePoint = SamplePoint(simulator,dataDir/"platform-files/sgbatch_validation_template.xml", [1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.0], 10_000_000_000, 0, {"test":(dataDir/"dataset-configs/crown_ttbar_testjob.json",dataDir/"workload-configs/crown_ttbar_testjob.json"),"copy":(dataDir/"dataset-configs/crown_ttbar_copyjob.json",dataDir/"workload-configs/crown_ttbar_copyjob.json")},data)
+	samplePoint = SamplePoint(simulator,dataDir/"platform-files/sgbatch_validation_template.xml", [1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.0], 10_000_000_000, 0, {"test":(dataDir/"dataset-configs/crown_ttbar_testjob.json",dataDir/"workload-configs/crown_ttbar_testjob.json"),"copy":(dataDir/"dataset-configs/crown_ttbar_copyjob.json",dataDir/"workload-configs/crown_ttbar_copyjob.json")},data,MRELoss)
 	best=None
 	bestLoss=None
-	for i in range(10):
-		for j in range(10):
-			stoptime=time()+3600
-			print(1/10**i,1/10**j)
-			calibrator = sc.calibrators.GradientDescent(1/10**i,1/10**j,early_reject_loss=1.0)
-			calibrator.add_param("cpuSpeed", sc.parameter.Exponential(20, 40).format("%.2f"))
-			calibrator.add_param("ramDisk", sc.parameter.Exponential(20, 40).format("%.2f"))
-			calibrator.add_param("disk", sc.parameter.Exponential(20, 33).format("%.2f"))
-			calibrator.add_param("internalNetwork", sc.parameter.Exponential(20, 33).format("%.2f"))
-			calibrator.add_param("externalFastNetwork", sc.parameter.Exponential(20, 33).format("%.2f"))
-			calibrator.add_param("externalSlowNetwork", sc.parameter.Exponential(20, 33).format("%.2f"))
+	#for i in range(10):
+	for j in range(10):
+		stoptime=time()+3600
+		print(0.01,100/10**j)
+		calibrator = sc.calibrators.GradientDescent(0.01,100/10**j,early_reject_loss=1.0)
+		calibrator.add_param("cpuSpeed", sc.parameter.Exponential(20, 40).format("%.2f"))
+		calibrator.add_param("ramDisk", sc.parameter.Exponential(20, 40).format("%.2f"))
+		calibrator.add_param("disk", sc.parameter.Exponential(20, 33).format("%.2f"))
+		calibrator.add_param("internalNetwork", sc.parameter.Exponential(20, 33).format("%.2f"))
+		calibrator.add_param("externalFastNetwork", sc.parameter.Exponential(20, 33).format("%.2f"))
+		calibrator.add_param("externalSlowNetwork", sc.parameter.Exponential(20, 33).format("%.2f"))
 
-			
-			#coordinator = sc.coordinators.ThreadPool(pool_size=args.cores) 
-			#maxs=samplePoint(	{"cpuSpeed":"1970Mf",	"disk":"17MBps", "ramDisk":"1GBps",	"internalNetwork":"10GBps",	"externalSlowNetwork":"1.15Gbps", "externalFastNetwork":"11.5Gbps"})
-			#print("Max's",maxs)
-			t0 = time()
-			#cal=calibrator.calibrate(samplePoint, 3600, coordinator=coordinator)
-			cal=calibrator.descend(samplePoint,{'cpuSpeed': 1959376102.6873443, 'ramDisk': 7960898256.745884, 'disk': 13264014.962423073, 'internalNetwork': 7958060335.116626, 'externalFastNetwork': 4258718314.2515693, 'externalSlowNetwork': 5966513.835689467},stoptime)
-			t1 = time()
-			#print ("We should now be printing the calibration")
-			print(cal)
-			print(t1-t0)
-			if best is None or cal[1]<bestLoss:
-				bestLoss=cal[1]
-				best=(1/10**i,1/10**j)
+		
+		#coordinator = sc.coordinators.ThreadPool(pool_size=args.cores) 
+		#maxs=samplePoint(	{"cpuSpeed":"1970Mf",	"disk":"17MBps", "ramDisk":"1GBps",	"internalNetwork":"10GBps",	"externalSlowNetwork":"1.15Gbps", "externalFastNetwork":"11.5Gbps"})
+		#print("Max's",maxs)
+		t0 = time()
+		#cal=calibrator.calibrate(samplePoint, 3600, coordinator=coordinator)
+		cal=calibrator.descend(samplePoint,{'cpuSpeed': 1959376102.6873443, 'ramDisk': 7960898256.745884, 'disk': 13264014.962423073, 'internalNetwork': 7958060335.116626, 'externalFastNetwork': 4258718314.2515693, 'externalSlowNetwork': 5966513.835689467},stoptime)
+		t1 = time()
+		#print ("We should now be printing the calibration")
+		print(cal)
+		print(t1-t0)
+		if best is None or cal[1]<bestLoss:
+			bestLoss=cal[1]
+			best=(0.01,100/10**j)
 	print(best,bestLoss)
 
 	
