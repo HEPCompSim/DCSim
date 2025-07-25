223792329c2085fcff12b257a63fb08a619b26f9
Maximilian Horzela <maximilian.horzela@cern.ch>
2025-07-25 12:31:39 +0000

    keep default values

--- tools/htcondor/validation_runs/run_simulations.sh
+++ tools/htcondor/validation_runs/run_simulations.sh
@@ -15,9 +15,6 @@ python3 run_shell_simulations.py \
     --platform "$1" \
     --workload "$2" \
     --dataset "$3" \
-    --xrootd_blocksize "$4" \
-    --storage_buffer_size "$5" \
-    --seed "$6" \
-    --shell "$7" \
-    --from-line "$8" \
-    --to-line "$9"
+    --shell "$4" \
+    --from-line "$5" \
+    --to-line "$6"
