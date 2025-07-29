3932909b35e498ad20bca832206ae379898a587e
Maximilian Horzela <maximilian.horzela@cern.ch>
2025-07-29 13:51:08 +0000

    debug

--- tools/htcondor/validation_runs/run_simulations.sh
+++ tools/htcondor/validation_runs/run_simulations.sh
@@ -12,9 +12,6 @@ echo "CURRENT DIRECTORY CONTENT:"
 ls $(pwd)
 
 python3 run_shell_simulations.py \
-    # --platform "$1" \
-    # --workload "$2" \
-    # --dataset "$3" \
     --shell "$1" \
     --from-line "$2" \
     --to-line "$3"
