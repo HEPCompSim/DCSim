d4f99e0c984e3ebae47d10ca4f331de39b8d8597
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2025-12-16 16:22:37 -1000

    Update simcal_calibrator.py

--- tools/simcal_calibrator.py
+++ tools/simcal_calibrator.py
@@ -200,7 +200,7 @@ class Simulator(sc.Simulator):
 			inter[workload] = {}
 			out[workload] = {}
 			for hitrate in self.hitrates:
-				i,o=self.dcsim(env,{"workload":self.workloads[workload], "platform":platform.name, "hitrate":hitrate,"xrootd_block":self.xrootd_blocksize,"network_blocksize":self.network_blocksize,"xrd_flops_per_time_local":args["xrd_flops_per_time_local"],"xrd_flops_per_time":args["xrd_flops_per_time"]},self.keep and (filestring+"_"+str(key)+"_"+str(hitrate)))
+				i,o=self.dcsim(env,{"workload":self.workloads[workload], "platform":platform.name, "hitrate":hitrate,"xrootd_block":self.xrootd_blocksize,"network_blocksize":self.network_blocksize,"xrd_flops_per_time_local":args["xrd_flops_per_time_local"],"xrd_flops_per_time":args["xrd_flops_per_time"]},self.keep and (filestring+"_"+str(workload)+"_"+str(hitrate)))
 				inter[workload][hitrate] = i
 				out[workload][hitrate] = o
 		platform.close()
