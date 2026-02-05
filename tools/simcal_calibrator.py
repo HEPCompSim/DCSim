a6bd35a866abb31b21984f66b45231b18eb9336e
Jesse McDonald <jamcdonald@lcmail.lcsc.edu>
2026-02-04 14:36:49 -1000

    Update simcal_calibrator.py

--- tools/simcal_calibrator.py
+++ tools/simcal_calibrator.py
@@ -921,7 +921,7 @@ if __name__=="__main__":
 		dataDir/"workload-configs/crown_ttbar_testjob.json"),
 		"copy":(dataDir/"dataset-configs/crown_ttbar_copyjob.json",
 		dataDir/"workload-configs/crown_ttbar_copyjob_no_cpu.json")}
-	if if args.only_slow:
+	if args.only_slow:
 		test_cases={}
 	if args.include_slow or args.only_slow::
 		test_cases["slow"]=(dataDir/"dataset-configs/crown_ttbar_slowjob.json",
