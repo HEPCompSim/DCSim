75d00bb5ceee4ca1ef3e2f3bebec35260f6e24cb
Henri Casanova <henric@hawaii.edu>
2026-06-10 14:50:24 -1000

    Warning--

--- src/Workload.cpp
+++ src/Workload.cpp
@@ -185,8 +185,8 @@ void Workload::assignFiles(std::vector<Dataset> const &dataset_specs) {
     // int num_files = all_files.size();
     size_t k = num_files / num_jobs;
     std::cerr << "Assigning " << num_files << " files to " << num_jobs << " jobs\n";
-    for (auto j = 0; j < num_jobs; ++j) {
-        auto beg_it = all_files.begin() + j * k;
+    for (size_t j = 0; j < num_jobs; ++j) {
+        auto beg_it = all_files.begin() + static_cast<std::ptrdiff_t>(j * k);
         if (std::distance(beg_it, all_files.end()) < k) {
             std::copy(beg_it, all_files.end(), std::back_inserter(job_batch[j].infiles));
             break;
